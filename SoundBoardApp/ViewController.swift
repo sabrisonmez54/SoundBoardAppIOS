31a1714e1e74bba06b66a3e6dc139bf92b8f1fb7
Sabri <sabri@AP265X7755.fios-router.home>
2019-02-08 20:06:05 -0500

    final commit

--- SoundBoardApp/ViewController.swift
+++ SoundBoardApp/ViewController.swift
@@ -7,13 +7,102 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+
+    var audioPlayer:AVAudioPlayer?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    
+    
+    
+    
+    @IBAction func atapped(_ sender: Any) {
+    
+        let url = Bundle.main.url(forResource: "a", withExtension: "mp3")
+        
+        guard url != nil else{
+            return
+        }
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+            
+        }
+        catch{
+            print("error")
+        }
+    }
+    
+    
+    @IBAction func bTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "b", withExtension: "mp3")
+        
+        guard url != nil else{
+            return
+        }
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+            
+        }
+        catch{
+            print("error")
+        }
+    
+    }
+    
+    
+    
+    @IBAction func cTapped(_ sender: Any) {
+    
+        let url = Bundle.main.url(forResource: "c", withExtension: "mp3")
+        
+        guard url != nil else{
+            return
+        }
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+            
+        }
+        catch{
+            print("error")
+        }
+    }
+
+    
+    
+    @IBAction func dTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "d", withExtension: "mp3")
+        
+        guard url != nil else{
+            return
+        }
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+            
+        }
+        catch{
+            print("error")
+        }
+    
+    }
+    
+
 
 
 }
